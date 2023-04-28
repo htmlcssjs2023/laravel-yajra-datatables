dcb48c678d6e0c5b5c21a83d1d4a72f173c87b06
mdabdulhalim <htmlcssjs2023>
2023-04-28 23:31:28 +0600

    Yajra DataTable in Laravel

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        $gender = $faker->randomElement(['male', 'female']);
+        foreach(range(1, 200) as $index){
+           DB::table('students')->insert([
+            'name' => $faker->name($gender),
+            'email' => $faker->email,
+            'username' => $faker->userName,
+            'phone' => $faker->phoneNumber,
+            'dob' => $faker->date($formate = 'Y-m-d', $max='now')
+           ]);
+        }
+    }
+}
